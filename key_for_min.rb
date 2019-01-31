6e777dfb340b6e8571832ed6b84342a32c1615b9
Adam Gordon <adamjgordon24@gmail.com>
2019-01-31 03:53:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,11 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
 name_hash.each do |key,value|
-  if  
-  
+  if  lowest_value == nil || value < lowest_value
+    lowest_key = key
+    lowest_value = value
   end
+  
+end
   lowest_key
 end
